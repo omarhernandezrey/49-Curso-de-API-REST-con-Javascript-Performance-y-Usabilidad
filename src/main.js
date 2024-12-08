ad11a61eb31ea520b21be0f60fe8ed2a94d1346f
omarhernandezrey <hernandezreyomar@gmail.com>
2024-12-08 12:20:58 -0500

    Mostrando y ocultando secciones

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ async function getTrendingMoviesPreview() {
   const movies = data.results;
 
   movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
     
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -27,7 +27,7 @@ async function getTrendingMoviesPreview() {
     );
 
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer);
+    trendingMoviesPreviewList.appendChild(movieContainer);
   });
 }
 
@@ -36,7 +36,7 @@ async function getCategegoriesPreview() {
   const categories = data.genres;
 
   categories.forEach(category => {
-    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
     
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
@@ -48,6 +48,6 @@ async function getCategegoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    previewCategoriesContainer.appendChild(categoryContainer);
+    categoriesPreviewList.appendChild(categoryContainer);
   });
 }
