bc376eaf429c38c2895552c9f61990dc8314263b
omarhernandezrey <hernandezreyomar@gmail.com>
2024-12-08 00:09:12 -0500

    Lista de categorías

--- src/main.js
+++ src/main.js
@@ -1,22 +1,48 @@
-async function getTrendingMoviesPreview() { // Define una función asíncrona para obtener y mostrar películas en tendencia.
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY); // Realiza una solicitud HTTP GET para obtener películas en tendencia.
-    const data = await res.json(); // Convierte la respuesta en un objeto JSON.
+async function getTrendingMoviesPreview() {
+  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+  const data = await res.json();
 
-    const movies = data.results; // Extrae la lista de películas de los resultados del objeto JSON.
-    movies.forEach(movie => { // Itera sobre cada película.
-      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList') // Busca el contenedor en el DOM.
-      
-      const movieContainer = document.createElement('div'); // Crea un nuevo div para cada película.
-      movieContainer.classList.add('movie-container'); // Añade una clase para estilizar el contenedor de la película.
-  
-      const movieImg = document.createElement('img'); // Crea un elemento img para la imagen de la película.
-      movieImg.classList.add('movie-img'); // Añade una clase para estilizar la imagen.
-      movieImg.setAttribute('alt', movie.title); // Establece el atributo alt con el título de la película.
-      movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path); // Configura la URL de la imagen como el src del img.
-  
-      movieContainer.appendChild(movieImg); // Agrega el elemento img al contenedor de la película.
-      trendingPreviewMoviesContainer.appendChild(movieContainer); // Agrega el contenedor de la película al contenedor principal en el DOM.
-    });
-  }
-  
-  getTrendingMoviesPreview(); // Llama a la función para ejecutarla al cargar el script.
+  const movies = data.results;
+  movies.forEach(movie => {
+    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
+}
+
+async function getCategegoriesPreview() {
+  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+  const data = await res.json();
+
+  const categories = data.genres;
+  categories.forEach(category => {
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+}
+
+getTrendingMoviesPreview();
+getCategegoriesPreview();
